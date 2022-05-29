34a222b64de32d66048676b1e1dbe9781fec2c7e
verillious <verilliousdev@gmail.com>
2022-05-29 15:32:57 +0100

    initial commit

---
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -0,0 +1,13 @@
+project = '{{ cookiecutter.project_name }}'
+copyright = '{{ cookiecutter.year }}, {{ cookiecutter.full_name }}'
+author = '{{ cookiecutter.full_name }}'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "single_page": True,
+    "use_fullscreen_button": False,
+}
+extensions = [
+    'm2r2'
+]
+source_suffix = ['.rst', '.md']
---
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.pkg_name}}.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Tests for `{{ cookiecutter.pkg_name }}` package."""
+
+{% if cookiecutter.command_line_interface|lower == 'click' -%}
+from click.testing import CliRunner
+
+from {{ cookiecutter.pkg_name }} import cli
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert "{{ cookiecutter.project_slug }}" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
+    assert help_result.exit_code == 0
+    assert "--help  Show this message and exit." in help_result.output
+{%- endif %}
